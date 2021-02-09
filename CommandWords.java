fd6a4f8b504610a3042b382fea315ca10dc0b26f
DanaBagundo <65426270+DanaBagundo@users.noreply.github.com>
2021-02-08 19:43:14 -0600

    Update CommandWords.java

--- CommandWords.java
+++ CommandWords.java
@@ -9,5 +9,20 @@
  * @author 52999
  */
 public class CommandWords {
-    
+    public CommandWords(){
+    }
+    //Array que contienen los comandos validos 
+    private static final String[] comandosValidos={
+        "go","quit!,!help
+    }
+    //Lo que se hace es checar cuanquier palabra que se de para revisar si es un comando valido
+    public boolean validarComando(String palabra){
+        for(int i=0; i<ComandosValidos.length;i++){
+            if(comandosValidos[i].equals(palabra)){
+                return true;
+            }
+        }
+        return false;
+    }
+                
 }
